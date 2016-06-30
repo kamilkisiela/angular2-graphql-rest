09c50e62d9ec4fb17d12fe53d8ab6b55e1082eda
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-30 16:51:38 +0200

    Step 1.8: Add Profiles model to the context

--- src/server/api/graphql/index.js
+++ src/server/api/graphql/index.js
@@ -5,10 +5,17 @@ import {
   schema
 } from './schema';
 
+import {
+  Profiles
+} from '../../sql/models';
+
 export default function(app) {
   app.use('/graphql', apolloServer((req) => ({
     graphiql: true,
     pretty: true,
+    context: {
+      Profiles: new Profiles
+    },
     schema,
     resolvers
   })));
