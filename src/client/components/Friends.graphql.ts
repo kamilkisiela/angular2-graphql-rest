3e0b0931e8d1372223222e2a308bee26ac1d569f
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-30 17:53:42 +0200

    Step 2.14: Update view

--- src/client/components/Friends.graphql.ts
+++ src/client/components/Friends.graphql.ts
@@ -14,23 +14,16 @@ import {
   client
 } from '../client';
 
-import {
-  DisplayFriendPipe
-} from '../pipes/DisplayFriendPipe';
-
 @Component({
   selector: 'friends',
   template: `
     <span class="gray">
       Friend with:
     </span>
-    <span *ngIf="friends" *ngFor="let friendId of friends" class="friend-with">
-      {{ getFriend(friendId) | displayFriend }}
+    <span *ngFor="let friend of data.profiles" class="friend-with">
+      {{ friend.fullName }}
     </span>
-  `,
-  pipes: [
-    DisplayFriendPipe
-  ]
+  `
 })
 @Apollo({
   client,
