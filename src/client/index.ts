89616c877b24bf33220efd66b91b8959edaea841
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-30 18:21:04 +0200

    Step 3.3: Remove router.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,21 +6,10 @@ import {
   bootstrap
 } from '@angular/platform-browser-dynamic';
 
-import {
-  HTTP_PROVIDERS
-} from '@angular/http';
-
 import './style.css';
 
 import {
   App
 } from './components/App';
 
-import {
-  APP_ROUTER_PROVIDERS
-} from './router';
-
-bootstrap(App, [
-  APP_ROUTER_PROVIDERS,
-  HTTP_PROVIDERS
-]);
+bootstrap(App);
